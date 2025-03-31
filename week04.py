172f93747b79ea818d84ef22292011dba51186df
seokhoon100 <100seokoon@gmail.com.kr>
2025-03-31 18:21:55 +0900

    v4.5 linkedList

--- week04.py
+++ week04.py
@@ -11,46 +11,61 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+
     def append(self, data):
         if not self.head:
             self.head = Node(data)
             return
         current = self.head
-        while current.link:
+        while current.next:
             current = current.link
         current.link = Node(data)
 
+
+    def remove(self, target):
+        current = self.head
+        if self.head.data == target:
+            self.head = self.head.link
+            current.link = None
+            return
+
+        previous = None
+        while current:
+            if target == current.data:
+                previous.link = current.link
+                current.link = None
+            previous = current
+            current = current.link
+
+
     def search(self, target):
         current = self.head
-        while current.link:
+        while current.next:
             if target == current.data:
                 return f"{target}을(를) 찾았습니다!"
             else:
-                current = current.link
+                current = current.next
         current.link = Node(target)
+
 
     def __str__(self):
         current = self.head
         result = ""
         while current is not None:
             result = result + f"{current.data} -> "
-            current = current.link
+            current = current.next
         return result + "END"
-        # return "Linked list!"
+
 
 ll = LinkedList()
-for _ in range(0, 20):
-    ll.append(random.randint(1, 20))
+ll.append(8)
+ll.append(10)
+ll.append(-9)
+
+
 print(ll)
+print(ll.search(99))
 print(ll.search(10))
+ll.remove(8)
+print(ll)
 
-
-
-# ll = LinkedList()
-# ll.append(8)
-# ll.append(10)
-# ll.append(-9)
-
-# print(ll)
-# print(ll.search(99))
-# print(ll.search(10))
