19dd59212db373662e9344c91eab47984db6b2fd
seokhoon100 <100seokoon@gmail.com.kr>
2025-03-31 17:45:16 +0900

    v4.3 linkedList

--- week04.py
+++ week04.py
@@ -16,11 +16,20 @@ class LinkedList:
             current = current.link
         current.link = Node(data)
 
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if target == current.data:
+                return f"{target}을(를) 찾았습니다!"
+            else:
+                current = current.link
+        current.link = Node(target)
+
     def __str__(self):
         current = self.head
+        result = ""
         while current is not None:
-            # print(current.data)
-            result = result + str(current.data) + " -> "
+            result = result + f"{current.data} -> "
             current = current.link
         return "END"
         # return "Linked list!"
@@ -32,3 +41,5 @@ ll.append(10)
 ll.append(-9)
 
 print(ll)
+print(ll.search(99))
+print(ll.search(10))
