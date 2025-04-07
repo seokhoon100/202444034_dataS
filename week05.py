a9161f94042fc1388530cd4da2b1204da8d71fa1
seokhoon100 <100seokoon@gmail.com.kr>
2025-04-07 17:50:04 +0900

    v5.3 Stack

--- week05.py
+++ week05.py
@@ -1,43 +1,20 @@
-class Node:
-    def __init__(self, data, link = None):
-        self.data = data
-        self.link = link
+from pickletools import string1
 
-class Stack:
-    def __init__(self):
-        self.top = None
-
-    def push(self, data):
-        node = Node(data)
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            # raise IndexError("스텍이 비어 있습니다.")
-            return "Stack is Empty!"
-        popped_node = self.top
-        self.top = self.top.link
-        popped_node.link = None
-        return popped_node.data
-
-    def peek(self):
-        return self.top.data
+s1 = []
+s1.append("Data structure")
+s1.append("Database")
+print(s1[-1])
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
 
 
 
-s1 = Stack()
-# print(s1.pop())
-s1.push("Data structure")
-s1.push("Database")
-# print(s1.pop())
-# print(s1.pop())
-print(s1.peek())
-for i in range(2):
-    print(s1.pop())
+
 
 
 
