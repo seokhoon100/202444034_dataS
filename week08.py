01ad0dfa248c1d7675d241fd721ee8fe0f42fcb6
seokhoon100 <100seokoon@gmail.com.kr>
2025-04-28 17:21:58 +0900

    v8.2_Node

--- week08.py
+++ week08.py
@@ -1,65 +1,61 @@
 class TreeNode:
-    def __init__(self):
-        self.left = None
-        self.data = None
-        self.right = None
+	def __init__(self):
+		self.left = None
+		self.data = None
+		self.right = None
+
+def pre_order(node):
+    if node is None:
+        return
+    print(node.data, end="->")
+    pre_order(node.left)
+    pre_order(node.right)
+
+
+def in_order(node):
+    if node is None:
+        return
+    in_order(node.left)
+    print(node.data, end="->")
+    in_order(node.right)
 
 
 def post_order(node):
-    if node:
-        post_order(node.left)
-        post_order(node.right)
-        print(node.data, end='->')
+    if node is None:
+        return
+    post_order(node.left)
+    post_order(node.right)
+    print(node.data, end="->")
 
 
-def insert(root, value):
-    new_node = TreeNode()
-    new_node.data = value
+node1 = TreeNode()
+node1.data = 'hs'
 
-    if root is None:  # 첫 번째 노드 처리
-        return new_node
+node2 = TreeNode()
+node2.data = 'sl'
+node1.left = node2
 
-    current = root
-    while True:
-        if value < current.data:
-            if current.left is None:
-                current.left = new_node
-                break
-            current = current.left  # move
-        else:
-            if current.right is None:
-                current.right = new_node
-                break
-            current = current.right  # move
-    return root
+node3 = TreeNode()
+node3.data = 'mb'
+node1.right = node3
 
+node4 = TreeNode()
+node4.data = 'hw'
+node2.left = node4
 
-if __name__ == "__main__":
-    numbers = [10, 15, 8, 3, 9]
-    # numbers = [10, 15, 8, 3, 9, 1, 7, 100]
-    root = None
+node5 = TreeNode()
+node5.data = 'zz'
+node2.right = node5
 
-    # 1번째 원소 부터 마지막 원소까지
-    for number in numbers:
-        root = insert(root, number)
+node6 = TreeNode()
+node6.data = 'sm'
+node3.left = node6
 
-    print("BST 구성 완료")
+# print(node6.data)
+# print(node1.right.left.data)
 
-    post_order(root)  # 3-9-8-15-10
-
-    # find_number = int(input())
-    # current = root
-    # while True:
-    #     if find_number == current.data:
-    #         print(f"{find_number}을(를) 찾았습니다")
-    #         break
-    #     elif find_number < current.data:
-    #         if current.left is None:
-    #             print(f"{find_number}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.left
-    #     else:
-    #         if current.right is None:
-    #             print(f"{find_number}이(가) 존재하지 않습니다")
-    #             break
-    #         current = current.right
+post_order(node1)
+print()
+pre_order(node1)
+print()
+in_order(node1)
