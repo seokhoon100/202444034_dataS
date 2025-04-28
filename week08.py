2ef4a5868db6bce5f47a34d4c435ed5071c81554
seokhoon100 <100seokoon@gmail.com.kr>
2025-04-28 18:08:44 +0900

    v8.3 Tree

--- week08.py
+++ week08.py
@@ -23,25 +23,19 @@ def post_order(node):
 
 
 class TreeNode:
-	def __init__(self):
-		self.left = None
-		self.data = None
-		self.right = None
+    def __init__(self):
+	    self.left = None
+	    self.data = None
+	    self.right = None
 
-# BST
-if __name__ == "__main__":
-    numbers = [10, 15, 8, 3, 9]
-    root = None
 
+def insert(root, value):
     node = TreeNode()
-    node.data = numbers[0]  # 10x`
-    root = node
+    node.data = value
 
-    for number in numbers[1:]:
-        node = TreeNode()
-        node.data = number
-
+    if root is Node:
         current = root
+
         while True:
             if number < current.data:
                 if current.left is None:
@@ -54,11 +48,8 @@ if __name__ == "__main__":
                     break
                 current = current.right  # 이동
 
-    print('BST 구성 완료')
-
-    post_order(root)
-
     find_number = int(input())
+
     current = root
     while True:
         if find_number == current.data:
@@ -74,3 +65,13 @@ if __name__ == "__main__":
                 print(f"{find_number}이(가) 존재하지 않습니다")
                 break
             current = current.right
+# BST
+if __name__ == "__main__":
+    numbers = [10, 15, 8, 3, 9, 100, 7, 13]
+    root = node
+
+    for number in numbers:
+        root = insert(root, number)
+
+    print('BST 구성 완료')
+    post_order(root)
