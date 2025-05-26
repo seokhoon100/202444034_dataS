c28fb685e3bbf967fec372a7a388ca87efbe32e6
seokhoon100 <100seokoon@gmail.com.kr>
2025-05-26 17:05:45 +0900

    v12.7

--- week12.py
+++ week12.py
@@ -124,14 +124,54 @@
 #     for c in range(G_self.SIZE):
 #         print(G_self.graph[r][c], end=' ')
 #     print()
+#
+# from collections import deque
+#
+# d = deque([91, 17, 33])
+#
+# d.append(-14)    #enqueue
+# # d.append(100)
+# d.append(99)
+#
+# for _ in range(len(d)):
+#     print(d.popleft())  #dequeue
 
-from collections import deque
+graph = [
+    [0, 1, 1, 0, 0, 0, 0, 0],   # 0 A
+    [1, 0, 0, 1, 0, 0, 0, 0],   # 1 B
+    [1, 0, 0, 1, 0, 0, 0, 0],   # 2 C
+    [0, 1, 1, 0, 1, 1, 1, 0],   # 3 D
+    [0, 0, 0, 1, 0, 1, 0, 0],   # 4 E
+    [0, 0, 0, 1, 1, 0, 0, 0],   # 5 F
+    [0, 0, 0, 1, 0, 0, 0, 1],   # 6 G
+    [0, 0, 0, 0, 0, 0, 1, 0]    # 7 H
+]
 
-d = deque([91, 17, 33])
+def dfs(g, i, visited):
+    visited[i] = True
+    print(chr(ord('A') + i), end = ' ')
+    for j in range(len(graph)):
+        if g[i][j] == 1 and not visited[j]:
+            dfs(g, j, visited)
 
-d.append(-14)    #enqueue
-# d.append(100)
-d.append(99)
+def bfs(g, i, visited):
+    queue = deque([i])
+    visited[i] = 1
+    # while len(queue) != 0:
+    while queue:
+        i = queue.popleft()
+        print(chr(ord('A') + i), end = ' ')
+        for j in range(len(graph)):
+            if g[i][j] == 1 and not visited[j]:
+                queue.append(j)
+                visited[j] + 1
+# 크루스칼 알고리즘
 
-for _ in range(len(d)):
-    print(d.popleft())  #dequeue
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [0 for _ in range(len(graph))]
+dfs(graph, 7, visited_dfs)
+print()
+bfs(graph, 4, visited_bfs)
+
+
+
